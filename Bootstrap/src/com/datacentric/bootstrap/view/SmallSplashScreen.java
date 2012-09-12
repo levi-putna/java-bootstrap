3c4a15963cfd073ea7131240c68101fe11f54b2c
levip <levip@benon.com>
2012-09-12 17:19:30 +1000

    Add Splash Screen

---
+++ Bootstrap/src/com/datacentric/bootstrap/view/SmallSplashScreen.java
@@ -0,0 +1,32 @@
+package com.datacentric.bootstrap.view;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class SmallSplashScreen {
+	public static void main(String[] arg) {
+		JWindow window = new JWindow();
+		window.getContentPane().add(
+				new JLabel("Loading JFrame...", SwingConstants.CENTER));
+		// Get the size of the screen
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		window.setBounds(200, 200, 200, 100);
+		window.setLocationRelativeTo(null);
+		
+		window.setVisible(true);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		window.setVisible(false);
+		JFrame frame = new JFrame();
+		frame.add(new JLabel("Welcome"));
+		frame.setVisible(true);
+		frame.setSize(300, 100);
+		window.dispose();
+
+	}
+}
